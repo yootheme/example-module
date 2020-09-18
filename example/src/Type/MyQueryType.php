b8b410fc5d25e2ba2876b0ce42a26fa26229c37c
janschoenherr <jan@yootheme.com>
2020-09-18 14:19:50 +0200

    cleanup

--- example/src/Type/MyQueryType.php
+++ example/src/Type/MyQueryType.php
@@ -12,7 +12,6 @@ class MyQueryType
 
                     'type' => 'MyType',
 
-                    // Arguments passed to the resolver function
                     'args' => [
 
                         'id' => [
@@ -23,30 +22,15 @@ class MyQueryType
 
                     'metadata' => [
 
-                        // Label in dynamic content select box
                         'label' => 'Custom MyType',
-
-                        // Option group in dynamic content select box
                         'group' => 'Custom',
 
-                        // Fields to input arguments in the customizer
                         'fields' => [
-
-                            // The array key corresponds to a key in the 'args' array above
                             'id' => [
-
-                                // Field label
                                 'label' => 'Type ID',
-
-                                // Field description
-                                'description' => 'Input a type ID.',
-
-                                // Default or custom field types can be used
-                                'type' => 'text'
-
+                                'description' => 'Input a type ID.'
                             ],
-
-                        ]
+                        ],
 
                     ],
 
