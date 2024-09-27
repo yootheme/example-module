4d2a777125c03858525de6fcdf4afe6647f57d19
janschoenherr <jan@yootheme.com>
2024-09-27 11:08:55 +0200

    update empty check

--- example/elements/example/templates/content.php
+++ example/elements/example/templates/content.php
@@ -1,3 +1,3 @@
-<?php if ($props['content']) : ?>
+<?php if ($props['content'] != '') : ?>
 <div><?= $props['content'] ?></div>
 <?php endif ?>
--- example/elements/example/templates/template.php
+++ example/elements/example/templates/template.php
@@ -7,7 +7,7 @@ $el = $this->el('div');
 
 <?= $el($props, $attrs) ?>
 
-    <?php if ($props['content']) : ?>
+    <?php if ($props['content'] != '') : ?>
         <?= $props['content'] ?>
     <?php endif ?>
 
